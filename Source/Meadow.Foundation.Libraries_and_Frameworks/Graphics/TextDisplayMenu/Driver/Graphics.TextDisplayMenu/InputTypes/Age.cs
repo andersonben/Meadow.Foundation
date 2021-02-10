1d59d7c91636af28d3600b3754cb624214487019
Adrian Stevens <adrian.stevens@gmail.com>
2021-02-09 23:10:50 -0800

    Add two new editable types to TextDisplayMenu

--- Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/TextDisplayMenu/Driver/Graphics.TextDisplayMenu/InputTypes/Age.cs
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/TextDisplayMenu/Driver/Graphics.TextDisplayMenu/InputTypes/Age.cs
@@ -2,7 +2,7 @@ namespace Meadow.Foundation.Displays.TextDisplayMenu.InputTypes
 {
     public class Age : NumericBase
     {
-        public Age(): base(0, 1000, 0)
+        public Age(): base(0, 200, 0)
         {
         }
     }
